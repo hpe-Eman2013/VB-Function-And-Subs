c97f78084b41ab8b6ea7281e26681459be31f3f3
Derek <derek.d.emanuel@hpe.com>
2016-09-02 10:07:52 -0400

    Update DisableCertainControls.vb

--- Subs/DisableCertainControls.vb
+++ Subs/DisableCertainControls.vb
@@ -1,9 +1,10 @@
-Public Sub DisableCertainControls(values As List(Of Control), container As Control)
+Public Sub DisableCertainControls(values As List(Of Control), container As Control, 
+	isDisabled As Boolean)
 	Dim allControls = valid.GetAllControls(container)
 	For Each item As Control In values
 		For Each con As Control In allControls
 			If item.Name.Equals(con.Name) Then
-				con.Enabled = False
+				con.Enabled = isDisabled
 				Exit For
 			End If
 		Next
